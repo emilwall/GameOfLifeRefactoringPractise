487f036f6601ff76e6104de5c7b72a71fe4caad2
Emil Wall <emil.wall@valtech.se>
2015-04-14 10:26:37 +0200

    Refactoring, created World class

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -10,139 +10,26 @@ namespace GameOfLife
 {
     class Program
     {
-        private static bool[,] b = new bool[12,12];
-        private static bool[,] b2 = new bool[12,12];
-
         static void Main(string[] args)
         {
-            var r = new Random();
-            for (int i = 1; i < 11; i++)
-            {
-                for (int j = 1; j < 11; j++)
-                {
-                    if (r.NextDouble() < 0.3)
-                    {
-                        b[i,j] = true;
-                    }
-                }
-            }
+            var world = new World();
 
-            for (int i = 0; i < 12; i++)
-            {
-                for (int j = 0; j < 12; j++)
-                {
-                    if (j == 0 || j == 11 || i == 0 || i == 11)
-                    {
-                        Console.Write("#");
-                    }
-                    else if(b[i,j])
-                    {
-                        Console.Write("*");
-                    }
-                    else
-                    {
-                        Console.Write("0");
-                    }
-                }
-                Console.WriteLine();
-            }
+            world.Print();
+
             Console.WriteLine("Press any key to start living!!!!!!!!");
             Console.ReadLine();
 
             var c = 0;
-            while(true)
+            while (true)
             {
                 Console.WriteLine("Generation " + c);
-                for (int i = 1; i < 11; i++)
-                {
-                    for (int j = 1; j < 11; j++)
-                    {
-                        if (b[i, j])
-                        {
-                            switch (check(i, j))
-                            {
-                                case 0:
-                                    b2[i, j] = false;
-                                    break;
-                                case 1:
-                                    b2[i, j] = false;
-                                    break;
-                                case 2:
-                                    b2[i, j] = true;
-                                    break;
-                                case 3:
-                                    b2[i, j] = true;
-                                    break;
-                                default:
-                                    b2[i, j] = false;
-                                    break;
-                            }
-                        }
-                        else
-                        {
-                            if (check(i, j) == 3)
-                            {
-                                b2[i, j] = true;
-                            }
-                        }
-                    }
-                }
-                b = b2;
+                world.StepGeneration();
+                world.Print();
 
-                for (int i = 0; i < 12; i++)
-                {
-                    for (int j = 0; j < 12; j++)
-                    {
-                        if (j == 0 || j == 11 || i == 0 || i == 11)
-                        {
-                            Console.Write("#");
-                        }
-                        else if (b[i, j])
-                        {
-                            Console.Write("*");
-                        }
-                        else
-                        {
-                            Console.Write("0");
-                        }
-                    }
-                    Console.WriteLine();
-                }
                 Thread.Sleep(1500);
                 c++;
                 Console.WriteLine("____________________________________________");
             }
-        }
-
-        private static int check(int i, int j)
-        {
-            var c = 0;
-            if (b[i-1,j-1])
-            {
-                c++;
-            }if (b[i-1,j])
-            {
-                c++;
-            }if (b[i,j-1])
-            {
-                c++;
-            }if (b[i+1,j-1])
-            {
-                c++;
-            }if (b[i-1,j+1])
-            {
-                c++;
-            }if (b[i,j+1])
-            {
-                c++;
-            }if (b[i+1,j])
-            {
-                c++;
-            }if (b[i+1,j+1])
-            {
-                c++;
-            }
-            return c;
         }
     }
 }
---
+++ GameOfLife/World.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class World
+    {
+        public World()
+        {
+            _b = new bool[12, 12];
+            b2 = new bool[12, 12];
+            Generate();
+        }
+
+        private bool[,] _b;
+        private bool[,] b2;
+
+        private void Generate()
+        {
+            var r = new Random();
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    if (r.NextDouble() < 0.3)
+                    {
+                        _b[i, j] = true;
+                    }
+                }
+            }
+
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    if (j == 0 || j == 11 || i == 0 || i == 11)
+                    {
+                        Console.Write("#");
+                    }
+                    else if (_b[i, j])
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void StepGeneration()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    if (_b[i, j])
+                    {
+                        switch (check(i, j))
+                        {
+                            case 0:
+                                b2[i, j] = false;
+                                break;
+                            case 1:
+                                b2[i, j] = false;
+                                break;
+                            case 2:
+                                b2[i, j] = true;
+                                break;
+                            case 3:
+                                b2[i, j] = true;
+                                break;
+                            default:
+                                b2[i, j] = false;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        if (check(i, j) == 3)
+                        {
+                            b2[i, j] = true;
+                        }
+                    }
+                }
+            }
+            _b = b2;
+        }
+
+        private int check(int i, int j)
+        {
+            var c = 0;
+            if (_b[i - 1, j - 1])
+            {
+                c++;
+            } if (_b[i - 1, j])
+            {
+                c++;
+            } if (_b[i, j - 1])
+            {
+                c++;
+            } if (_b[i + 1, j - 1])
+            {
+                c++;
+            } if (_b[i - 1, j + 1])
+            {
+                c++;
+            } if (_b[i, j + 1])
+            {
+                c++;
+            } if (_b[i + 1, j])
+            {
+                c++;
+            } if (_b[i + 1, j + 1])
+            {
+                c++;
+            }
+            return c;
+        }
+    }
+}
