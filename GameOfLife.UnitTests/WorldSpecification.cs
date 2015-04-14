b51505b1a863e19a53f6a63687adf71337f778b7
Emil Wall <emil.wall@valtech.se>
2015-04-14 10:47:15 +0200

    Verify percentage of alive cells in first generation

--- GameOfLife.UnitTests/WorldSpecification.cs
+++ GameOfLife.UnitTests/WorldSpecification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace GameOfLife.UnitTests
@@ -13,6 +14,24 @@ namespace GameOfLife.UnitTests
 
             Assert.That(world.CurrentWorld.Length, Is.EqualTo(144));
             Assert.That(world.CurrentWorld[7, 7], Is.Not.Null);
+        }
+
+        [Test]
+        public void InitialWorldShouldHaveAround30PercentAliveCells()
+        {
+            var initialWorld = new World().CurrentWorld;
+
+            var aliveCells = 0;
+            foreach (var b in initialWorld)
+            {
+                if (b)
+                {
+                    aliveCells++;
+                }
+            }
+
+            Assert.That(aliveCells, Is.GreaterThan(144 * 0.1));
+            Assert.That(aliveCells, Is.LessThan(144 * 0.5));
         }
     }
 }
